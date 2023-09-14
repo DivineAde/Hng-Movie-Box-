e808943cdea5c47add39de29fbb30259b163a4a5
Divine Adeyeye <87317463+DivineAde@users.noreply.github.com>
2023-09-14 09:13:29 +0100

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
   return (
     <div className=" bg-white text-black flex flex-col items-center justify-center h-screen">
       <h1 className="text-xl">Oooops, Page not found </h1>
-      <Link href={"/"}>
+      <Link href='/'>
         <button className=" bg-blue-700 py-1 px-2 text-white rounded-lg font-semibold">
           Go back home
         </button>
